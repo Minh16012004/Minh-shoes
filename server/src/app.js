62bc4f4634e7f120b43ef7aa75eaa63c1ba08062
Harry-Minh <phungonzai@gmail.com>
2026-01-06 00:33:54 +0700

    Hoàn thiện auth + phân quyền admin

--- server/src/app.js
+++ server/src/app.js
@@ -1,14 +1,16 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const connectDB = require('./config/db')
-require('dotenv').config()
 
 const app = express()
 
 connectDB()
-app.use(express.urlencoded({ extended: true }))
+
 app.use(cors())
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use('/uploads', express.static('uploads'))
 
 app.use('/api/auth', require('./routes/auth.route'))
 
--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -1,14 +1,26 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-  name: String,
+  name: {
+    type: String,
+    required: true
+  },
   email: {
     type: String,
+    required: true,
     unique: true
   },
-  password: String,
+  password: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String, // lưu URL ảnh
+    default: ''
+  },
   role: {
     type: String,
+    enum: ['user', 'admin'],
     default: 'user'
   }
 }, { timestamps: true })
