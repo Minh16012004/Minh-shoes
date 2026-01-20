7a43e6590798bab16a6d828f05cf166f42015c96
Harry-Minh <phungonzai@gmail.com>
2026-01-21 02:49:03 +0700

    ok

--- server/app.js
+++ server/app.js
@@ -16,7 +16,9 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin:[ 'http://localhost:5173',
+  "https://minh-shoes-37dt.vercel.app"
+  ],
   credentials: true
 }));
 app.use(express.json());
